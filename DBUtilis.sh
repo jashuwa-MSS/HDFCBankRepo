914eaf24199dce53cfa3471ef395eac206004a67
Jashuwavr <123135632+Jashuwavr@users.noreply.github.com>
2023-02-19 14:18:40 +0530

    Update DBUtilis.sh

--- DBUtilis.sh
+++ DBUtilis.sh
@@ -3,3 +3,5 @@ Hello,
 This is the source code in master branch.
 
 This is the second source code in development branch.
+
+I am updating souce code in DBUtilis file in master branch inorder to pull the code.
